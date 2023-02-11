86b56ca983bf6b0eacc7b1ef6d98129c7c5a0d60
Ricardo Ariel Maya <rickymayita@hotmail.com>
2023-02-10 22:16:25 -0300

    correccion feet

--- Font/src/components/Detail.jsx
+++ Font/src/components/Detail.jsx
@@ -7,7 +7,7 @@ export default function Detail(props) {
     const [character, setCharacter] = React.useState({});
 
     useEffect(() => {
-        fetch(`http://localhost:3001/rickandmorty/character/${detailId}`)
+        fetch(`http://localhost:3001/rickandmorty/detail/${detailId}`)
             .then((response) => response.json())
             .then((char) => {
                 if (char.name) {
@@ -29,6 +29,7 @@ export default function Detail(props) {
                 <div className={styles.bodyId}>
                     <div className={styles.imgId}><img src={character.image} alt="" /></div>
                     <div className={styles.infId}>
+                        {console.log(character)}
                         <h2>Status: {character.status}</h2>
                         <h2>Species: {character.species}</h2>
                         <h2>Gender: {character.gender}</h2>
